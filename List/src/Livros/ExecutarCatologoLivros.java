52bc4e44ed0fc8610f352c9bf1276d0b7087efd8
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-13 23:10:49 -0300

    Finalizacao de todos projetos e desafios

---
+++ List/src/Livros/ExecutarCatologoLivros.java
@@ -0,0 +1,8 @@
+package List.OperacoesBasicas.Livros;
+
+public class ExecutarCatologoLivros {
+    public static void main(String[] args) {
+        CatologoLivros catologoLivros = new CatologoLivros();
+        catologoLivros.executarAplicacao();
+    }
+}
---
+++ List/src/Tarefas/TestTarefas.java
@@ -0,0 +1,6 @@
+package List.OperacoesBasicas.Tarefas;
+
+public class TestTarefas {
+    public static void main(String[] args) {
+    }
+}
